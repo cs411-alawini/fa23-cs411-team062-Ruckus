dd0bd77471399e02d1b6202966b998e4be6be422
Tyler Hirsch <70651139+THirsch62@users.noreply.github.com>
2023-12-04 13:47:13 -0600

    Create Match.sql
    Start of match file

---
+++ Match.sql
@@ -0,0 +1,25 @@
+/*
+Attributes
+  1)  Track_id
+  2)  Artists
+  3)  Album_name
+  4)  Track_name
+  5)  Popularity
+  6)  Duration_ms
+  7)  Explicit
+  8)  Danceability
+  9)  Energy
+  10) Key
+  11) Loudness
+  12) Mode
+  13) Speechiness
+  14) Acousticness
+  15) Instrumentalness
+  16) Liveness
+  17) Valence
+  18) Tempo
+  19) Time_signature
+  20) Track_genre
+Can match on attributes 6-20
+*/
+
